f37e24de16cd0c01d579f036596a1825da75d977
Ankit25akofficial <ankitkumar252508@gmail.com>
2026-06-25 10:51:38 +0530

    Study update: 6/25/2026

--- day02/ok.js
+++ day02/ok.js
@@ -21,13 +21,27 @@
 //   console.log("hey!");
 // }
 
-sayHi();
+// sayHi();
 
-function sayHi() {
-  console.log("hey!");
-  console.log(add(10, 2));
+// function sayHi() {
+//   console.log("hey!");
+//   console.log(add(10, 2));
+// }
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+function outer() {
+  const outerVar = "Hey I am the outer Var";
+
+  function inner() {
+    const innerVar = "hey I am an inner var";
+    console.log(innerVar);
+    console.log(outerVar);
+  }
+
+  inner();
 }
 
-function add(a, b) {
-  return a + b;
-}
+outer();
