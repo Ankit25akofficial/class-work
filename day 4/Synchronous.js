54dfdbd01d3deb5bd21cbda14900f3c6afaf35de
Ankit25akofficial <ankitkumar252508@gmail.com>
2026-07-01 09:18:59 +0530

    Synchronous

---
+++ day 4/Synchronous.js
@@ -0,0 +1,15 @@
+function task1() {
+  console.log("Task 1 completed");
+}
+
+function task2() {
+  console.log("Task 2 completed");
+}
+
+function task3() {
+  console.log("Task 3 completed");
+}
+
+task1();
+task2();
+task3();
