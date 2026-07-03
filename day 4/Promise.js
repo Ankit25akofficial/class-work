1712f41733bf21304a6a769050a545ca4728b798
Ankit25akofficial <ankitkumar252508@gmail.com>
2026-07-03 09:13:34 +0530

    Promise

--- day 4/Promise.js
+++ day 4/Promise.js
@@ -1,15 +1,58 @@
 // A Promise is used to handle asynchronous operations. It promises to return a result later, either successfully or with an error....
 
-function downloadFile() {
-  return new Promise((resolve) => {
-    console.log("Downloading file...");
+// function downloadFile() {
+//   return new Promise((resolve) => {
+//     console.log("Downloading file...");
 
+//     setTimeout(() => {
+//       resolve("Download completed");
+//     }, 2000);
+//   });
+// }
+
+// downloadFile().then((message) => {
+//   console.log(message);
+// });
+
+function login() {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      resolve("Download completed");
-    }, 2000);
+      resolve("Login Successful");
+    }, 1000);
   });
 }
 
-downloadFile().then((message) => {
-  console.log(message);
-});
+function getUser() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ id: 101, name: "Ankit" });
+    }, 1000);
+  });
+}
+
+function getOrders(userId) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(["Laptop", "Phone"]);
+    }, 1000);
+  });
+}
+
+login()
+  .then((message) => {
+    console.log(message);
+    return getUser();
+  })
+  .then((user) => {
+    console.log(user);
+    return getOrders(user.id);
+  })
+  .then((orders) => {
+    console.log(orders);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log("Finished");
+  });
