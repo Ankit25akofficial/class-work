845b770d349b7fa79cc589103d9e74b8d5b07bfd
Ankit25akofficial <ankitkumar252508@gmail.com>
2026-07-20 20:40:19 +0530

    updated

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
